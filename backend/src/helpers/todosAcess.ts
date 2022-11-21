9cf69354d42d9f7e3ab2a264b4d8d04407557a61
Kubiat Morgan <msmahrt@gmail.com>
2022-11-21 10:55:24 +0100

    fix: ray tracing for dynamoDB

--- backend/src/helpers/todosAcess.ts
+++ backend/src/helpers/todosAcess.ts
@@ -1,17 +1,19 @@
 import * as AWS from 'aws-sdk'
-// import * as AWSXRay from 'aws-xray-sdk'
 import { DocumentClient } from 'aws-sdk/clients/dynamodb'
 import { createLogger } from '../utils/logger'
 import { TodoItem } from '../models/TodoItem'
 import { TodoUpdate } from '../models/TodoUpdate';
+import { getDocumentClient } from '@shelf/aws-ddb-with-xray';
 
-// const XAWS = AWSXRay.captureAWS(AWS)
 const logger = createLogger('TodosAccess')
 
 // TODO: Implement the dataLayer logic
 export class TodosAccess {
   constructor(
-    private readonly docClient: DocumentClient = new AWS.DynamoDB.DocumentClient(),
+    private readonly docClient: DocumentClient = getDocumentClient({
+      ddbParams: { region: 'us-east-1' },
+      ddbClientParams: { region: 'us-east-1' },
+    }),
     private readonly todosTable = process.env.TODOS_TABLE,
     private readonly todosIndex = process.env.INDEX_NAME) {
   }
