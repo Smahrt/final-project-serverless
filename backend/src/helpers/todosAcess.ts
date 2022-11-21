be373ff45701fe4051de27007c98a38770b3844d
Kubiat Morgan <msmahrt@gmail.com>
2022-11-21 11:26:52 +0100

    chore: fix unused vars

--- backend/src/helpers/todosAcess.ts
+++ backend/src/helpers/todosAcess.ts
@@ -1,4 +1,3 @@
-import * as AWS from 'aws-sdk'
 import { DocumentClient } from 'aws-sdk/clients/dynamodb'
 import { createLogger } from '../utils/logger'
 import { TodoItem } from '../models/TodoItem'
